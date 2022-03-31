2fa86f42f0a182485c7d1a02ce5fa120342f9354
Philipp Friedrich <freezer05@googlemail.com>
2022-03-31 12:09:22 +0200

    erste session

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <div className="text-3xl font-light text-center py-4">
+      nextlevelcommunication
+    </div>
+  );
+}
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Header></Header>
+      <main className="px-2">
+        <Component {...pageProps} />
+      </main>
+      <Footer></Footer>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
